23b2bc6bfc1687bf2f9304471ba8065fd687c06a
mmelendez3 <mele0019@gmail.com>
2021-11-06 20:45:41 -0500

    added delete option

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const { notes } = require('./db/db');
 const fs = require('fs');
 const path = require('path');
 
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//i have to check if i need this because the css loaded without it and the module said it shouldn't but it did.
+app.use(express.static('public'));
 
 
 
-//function to accept POST route's req.body value and the array we want to add the data to.
+// function to accept POST route's req.body value and the array we want to add the data to.
 function createNewNote(body, notesArray) {
     console.log(body);
     // our function's main code will go here!
@@ -43,6 +46,33 @@ app.post('/api/notes', (req, res) => {
   const note = createNewNote(req.body, notes);
     
     res.json(note);
+  });
+
+
+// delete notes
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let note;
+
+    notes.map((element, index) => {
+      if (element.id == id){
+        note = element
+        notes.splice(index, 1)
+        return res.json(note);
+      } 
+    
+    })
+});
+
+
+  //route to serve index.html
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  //route to notes.html
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
 
